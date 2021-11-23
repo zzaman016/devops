23e56586d6bb6b772c260a5f3892ab8dfb1127e4
Zamanbek Maxutbekov <zmaxutbekov@gmail.com>
2021-11-24 01:51:30 +0600

    feat: check user adultness

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ name = input('What is u name, man?')
 print(f'Hi, {name}!')
 
 age = input(f'How old are you, {name}?')
+
+if int(age)>18:
+    print('You are not allowed to be here')
+else:
+    print('Welcome')
